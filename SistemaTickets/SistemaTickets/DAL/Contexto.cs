d58b9dc024a1ceefc87d1452fae78a2d85bc0f67
aaron-developer25 <atminifg655@gmail.com>
2024-01-30 21:57:02 -0400

    Instalando herramientas necesarias como: Entity Fw y creacion del contexto.

---
+++ SistemaTickets/SistemaTickets/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaTickets.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
